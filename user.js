d79a9e740d7486e726272d1cec623f4f36c3034a
Schmorfil <69549339+Schmorfil@users.noreply.github.com>
2021-10-19 19:30:47 -0500

    Add files via upload

---
+++ user.js
@@ -0,0 +1,3 @@
+user_pref("capability.policy.policynames", "localfilelinks");
+user_pref("capability.policy.localfilelinks.sites", "http://sam");
+user_pref("capability.policy.localfilelinks.checkloaduri.enabled", "allAccess");
