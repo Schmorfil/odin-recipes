63f21f5e731127a10df02e6580ecbe5fdbbda69a
Schmorfil <69549339+Schmorfil@users.noreply.github.com>
2021-10-19 19:40:02 -0500

    Delete user.js

--- user.js
+++
@@ -1,3 +0,0 @@
-user_pref("capability.policy.policynames", "localfilelinks");
-user_pref("capability.policy.localfilelinks.sites", "http://sam");
-user_pref("capability.policy.localfilelinks.checkloaduri.enabled", "allAccess");
